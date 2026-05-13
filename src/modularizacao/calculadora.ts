4a0589f4b599e1d172a4ff624d7e0f668cd6ab77
anthonymenezes126-wq <anthonymenezes126@gmail.com>
2026-05-13 10:25:03 -0300

    Create calculadora.ts
    modularização

---
+++ src/modularizacao/calculadora.ts
@@ -0,0 +1,6 @@
+function soma(num1:number, num2:number):any{
+    console.log(`SOMA: ${num1 + num2}`)
+}
+function menos(num1:number, num2:number):any{
+    console.log(`SUBTRAÇÃO: ${num1 - num2}`)
+}
